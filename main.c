627e7bff0a00c4c1b25a8636a6bafa40498a1d1d
Prayag Desai <prayag2010@live.com>
2020-04-15 14:50:29 +0100

    Added line draw functions

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "stdint.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
 #include "epd1in54.h"
 #include "epdif.h"
 
 uint8_t imageBuf[5000] = {0};
 uint8_t imageBuffer[200][25];
+
+#define DISP_WHITE 0
+#define DISP_BLACK 1
 
 #define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
 #define BYTE_TO_BINARY(byte)  \
@@ -18,11 +22,11 @@ uint8_t imageBuffer[200][25];
   (byte & 0x02 ? '1' : '0'), \
   (byte & 0x01 ? '1' : '0') 
 
-void drawPixel(uint8_t x, uint8_t y)
+void drawPixel(uint8_t x, uint8_t y, bool color)
 {
     if(x > 199 || y > 199)
     {
-        printf("drawPixel: out of bound for X:%d Y:%d\n", x, y);
+        printf("%s: out of bound for X:%d Y:%d\n", __func__, x, y);
         return;
     }
 
@@ -31,7 +35,11 @@ void drawPixel(uint8_t x, uint8_t y)
     xPos = x / 8;
     xBitPos = x % 8;
     
-    imageBuffer[y][xPos] &= ~(1 << (7 - xBitPos));
+    if(color)
+        imageBuffer[y][xPos] &= ~(1 << (7 - xBitPos));
+    else
+        imageBuffer[y][xPos] |= (1 << (7 - xBitPos));
+
     // imageBuffer[y][xPos] = 0xFF;
     // uint8_t bit = (1 << (7 - xBitPos));
 
@@ -39,6 +47,43 @@ void drawPixel(uint8_t x, uint8_t y)
     // printf("For X: %d Y:%d | Binary: "BYTE_TO_BINARY_PATTERN"\n", x, y, BYTE_TO_BINARY(bit));
     
 }
+
+void drawLineX(uint16_t startPosX, uint16_t length, uint16_t startPosY, bool color)
+{
+    if(startPosX > 199 || startPosY > 199)
+    {
+        printf("%s: out of bound for X:%d Y:%d\n", __func__, startPosX, startPosY);
+        return;
+    }
+
+    for(int i = 0; i < length; i++)
+    {
+        if(startPosX + i > 199)
+        {
+            printf("%s: Warning: Line write out of bound for X:%d Y:%d at length: %d", __func__, startPosX, startPosY, startPosX + i);
+        }
+        drawPixel(startPosX + i, startPosY, color);
+    }
+}
+
+void drawLineY(uint16_t startPosX, uint16_t length, uint16_t startPosY, bool color)
+{
+    if(startPosX > 199 || startPosY > 199)
+    {
+        printf("%s: out of bound for X:%d Y:%d\n", __func__, startPosX, startPosY);
+        return;
+    }
+
+    for(int i = 0; i < length; i++)
+    {
+        if(startPosY + i > 199)
+        {
+            printf("%s: Warning: Line write out of bound for X:%d Y:%d at length: %d", __func__, startPosX, startPosY, startPosX + i);
+        }
+        drawPixel(startPosX, startPosY + i, color);
+    }
+}
+
 
 int main(void)
 {
@@ -93,12 +138,15 @@ int main(void)
 
     for(int i = 0; i < 200; i++)
     {
-        drawPixel(i, i);
+        drawPixel(i, i, DISP_BLACK);
         // printf("Draw for: %d\n", i);
     }
     // for (int i = 0; i < 200 / 8 * 200; i++) {
     //     SendData(imageBuf[i]);
     // } 
+    
+    drawLineX(100, 50, 10, DISP_BLACK);
+    drawLineY(10, 100, 100, DISP_BLACK);
 
     SetMemoryArea(0, 0, 200 - 1, 200 - 1);
     SetMemoryPointer(0, 0);
